8bc7d918dff252e9ddf0139caca3040f370c2da7
blackalegator <blackalegator@gmail.com>
2016-09-08 01:09:49 +0300

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
 // Package tlit is a package for encoding russian and ukrainian letters with latin ones.
-package tlit // import "github.com/runningmaster/tlit"
+package tlit // import "github.com/blackalegator/tlit"
